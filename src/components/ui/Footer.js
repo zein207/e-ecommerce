ff2162831f6ebc885f19c6114ecd7c36c18cd90d
David <zein207@gmail.com>
2022-04-04 18:53:50 -0500

    Footer Newsletter

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Newsletter } from './Newsletter'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer__main-container mt-5'>
+      <Newsletter />
+    </div>
   )
 }
---
+++ src/components/ui/Newsletter.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Newsletter = () => {
+  return (
+    <div className='newsletter__main-container'>
+        <h3>Newsletter</h3>
+        <h4>Subscribe to our newsletter</h4>
+
+        <form className='newsletter__form'>
+
+            <input
+                type="text"
+                placeholder="Enter your email"
+                className="form-control newsletter__input"
+                name="email"
+                autoComplete="off"
+            />
+            <button className='newsletter__icon'>
+                <i class="fa-solid fa-envelope"></i>
+            </button>
+        </form>
+    </div>
+  )
+}
